bc8fc6cdf12a416d6b23af8589c4d4d3e0481442
Ravivadaliya <vadaliyaravi2@gmail.com>
2024-01-11 17:06:46 +0530

    Add project files.

---
+++ Task2/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textsource = new System.Windows.Forms.TextBox();
+            this.textdestination = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(616, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 42);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Submit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(23, 162);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "All Files";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textsource
+            // 
+            this.textsource.Location = new System.Drawing.Point(173, 94);
+            this.textsource.Name = "textsource";
+            this.textsource.Size = new System.Drawing.Size(366, 22);
+            this.textsource.TabIndex = 3;
+            this.textsource.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textdestination
+            // 
+            this.textdestination.Location = new System.Drawing.Point(890, 95);
+            this.textdestination.Name = "textdestination";
+            this.textdestination.Size = new System.Drawing.Size(363, 22);
+            this.textdestination.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(810, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Destination";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(387, 342);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(692, 47);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1053, 305);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "0%";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(545, 94);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(39, 23);
+            this.button21.TabIndex = 8;
+            this.button21.Text = "....";
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1259, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "....";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(926, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 11;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip2.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip2_Opening);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(23, 206);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(242, 225);
+            this.checkedListBox1.TabIndex = 16;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1170, 188);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(236, 251);
+            this.textBox4.TabIndex = 17;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Location = new System.Drawing.Point(902, 305);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(0, 16);
+            this.TimeLabel.TabIndex = 19;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(616, 266);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(135, 43);
+            this.ClearButton.TabIndex = 20;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 559);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textdestination);
+            this.Controls.Add(this.textsource);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textsource;
+        private System.Windows.Forms.TextBox textdestination;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ Task2/Form1.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+using File = System.IO.File;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        private static int Total_Files = 0;
+        private static int Total_Folder = 1;
+        private static string[] stringArray = {} ;
+    
+        BackgroundWorker worker = new BackgroundWorker();
+       
+        public Form1()
+        {
+            InitializeComponent();
+            worker.WorkerSupportsCancellation = true;
+            worker.WorkerReportsProgress = true;
+            worker.ProgressChanged += Worker_ProgressChange;
+            worker.DoWork += Worker_DoWork;
+        }
+
+        private void Worker_ProgressChange(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label3.Text = progressBar1.Value.ToString()+"%";
+            if(progressBar1.Value == 100)
+            {
+                progressBar1.Value = 0;
+                label3.Text = progressBar1.Value.ToString();
+                MessageBox.Show($"File copy completed with {Total_Files} Files and  {Total_Folder} Folders");
+            }
+        }
+        private void Worker_DoWork(object sender,DoWorkEventArgs e)
+        {
+            button3.Invoke((MethodInvoker)delegate
+            {
+                button3.Enabled = false;
+            });
+            button3.Enabled = false;
+            CopyMethod(textsource.Text, textdestination.Text);
+            button3.Invoke((MethodInvoker)delegate
+            {
+                button3.Enabled = true;
+            });            
+        }
+
+        //submit button click
+        private void button1_Click(object sender, EventArgs e)
+        {
+            worker.RunWorkerAsync();
+        }
+
+        static bool IsFilePath(string path)
+        {
+            // Check if the path is a valid file path and the file exists
+            return !string.IsNullOrEmpty(path) && File.Exists(path);
+        }
+        private void CopyMethod(string target, string destination)
+        {
+            if (string.IsNullOrEmpty(target) || string.IsNullOrEmpty(destination))
+            {   
+                MessageBox.Show("Please select source and destination folders.");
+                return;
+            }
+
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+            // Get all files in the source folder
+            if (string.IsNullOrEmpty(target))
+            {
+                return;
+            }
+            if (stringArray.Length > 0)
+            {
+
+                int totallength = 0;
+                int filelength = 0;
+                int folderlenght = 0;
+
+                foreach (string item in stringArray)
+                {
+                    if (IsFilePath(item))
+                    {
+                        filelength++;
+                    }
+                    else
+                    {
+                        folderlenght++;
+                    }
+                }
+                totallength = filelength + folderlenght;
+                int processedItems = 0;
+                DateTime startTime = DateTime.Now;
+                foreach (string item in stringArray)
+                {
+                    if (string.IsNullOrEmpty(item))
+                    {
+                        continue;
+                    }
+                    if (File.Exists(item))
+                    {
+                        processedItems++;
+                        Total_Files++;
+                        string fileName = Path.GetFileName(item);
+                        string destinationFilePath = Path.Combine(destination, fileName);
+                        File.Copy(item, destinationFilePath, true); // true overwrite existing files
+                        int progressPercentage = (processedItems * 100) / totallength;
+                        worker.ReportProgress(progressPercentage);
+                    }
+                    else
+                    {
+                        processedItems++;
+                        Total_Folder++;
+                        string directoryName = Path.GetFileName(item);
+                        string destinationDirectoryPath = Path.Combine(destination, directoryName);
+                        CopyDirectory(item, destinationDirectoryPath);
+
+                        int progressPercentage = (processedItems * 100) / totallength;
+                        worker.ReportProgress(progressPercentage);
+                    }
+                }
+                DateTime endTime = DateTime.Now;
+                TimeSpan duration = endTime - startTime;
+                TimeLabel.Invoke((MethodInvoker)delegate
+                {
+                    TimeLabel.Text = duration.TotalSeconds.ToString() + "seconds";
+                });
+                progressBar1.Invoke((MethodInvoker)delegate
+                {
+                    progressBar1.Value = 100;
+                });
+                label3.Invoke((MethodInvoker)delegate
+                {
+                    label3.Text = "100";
+                });
+
+            }
+            else
+            {
+                string[] files = Directory.GetFiles(target);
+                string[] directories = Directory.GetDirectories(target);
+
+                int totalItems = files.Length + directories.Length;
+                int processedItems = 0;
+
+                DateTime startTime = DateTime.Now;
+                foreach (string file in files)
+                {
+                    processedItems++;
+                    Total_Files++;
+                    string fileName = Path.GetFileName(file);
+                    string destinationFilePath = Path.Combine(destination, fileName);
+                    File.Copy(file, destinationFilePath, true); // true overwrite existing files
+
+                    int progressPercentage = (processedItems * 100) / totalItems;
+                    worker.ReportProgress(progressPercentage);
+                }
+                // subdirectories
+                foreach (string directory in directories)
+                {
+                    processedItems++;
+                    Total_Folder++;
+                    string directoryName = Path.GetFileName(directory);
+                    string destinationDirectoryPath = Path.Combine(destination, directoryName);
+                    CopyDirectory(directory, destinationDirectoryPath);
+                    int progressPercentage = (processedItems * 100) / totalItems;
+                    worker.ReportProgress(progressPercentage);
+                }
+                DateTime endTime = DateTime.Now;
+                TimeSpan duration = endTime - startTime;
+                TimeLabel.Invoke((MethodInvoker)delegate
+                {
+                    TimeLabel.Text = duration.TotalSeconds.ToString() + "seconds";
+                });
+            }
+
+           
+        }
+
+        static void CopyDirectory(string source, string destination)
+        {
+            // Create the destination directory if not exiest
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+
+            string[] files = Directory.GetFiles(source);
+            string[] directories = Directory.GetDirectories(source);
+
+            // Copy files
+            foreach (string file in files)
+            {
+                Total_Files++;
+                string fileName = Path.GetFileName(file);
+                string destinationFilePath = Path.Combine(destination, fileName);
+                File.Copy(file, destinationFilePath, true);  // true overwrite existing filess
+            }
+
+            // recursion subdirectories
+            foreach (string directory in directories)
+            {
+                Total_Folder++;
+                string directoryName = Path.GetFileName(directory);
+                string destinationDirectoryPath = Path.Combine(destination, directoryName);
+                CopyDirectory(directory, destinationDirectoryPath);
+            }
+        }
+
+        #region unnecessary methods
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+        }
+        #endregion
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string selectedPath = textsource.Text;
+
+            // Check if the selected path is a folder
+            if (Directory.Exists(selectedPath))
+            {
+                string[] filesAndFolders = Directory.GetFileSystemEntries(selectedPath);
+
+                checkedListBox1.Items.Clear();
+                foreach (string path in filesAndFolders)
+                {
+                    checkedListBox1.Items.Add(path);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid folder path.");
+            }   
+        }
+        //when you select all file then this method called
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, checkBox1.Checked);
+            }
+            checkedListBox1.Enabled = !checkBox1.Checked;
+            textBox4.Text = "";
+            stringArray = null;
+            foreach (object item in checkedListBox1.CheckedItems)
+            {
+                string checkedItemValue = item.ToString();
+                textBox4.Text += checkedItemValue + Environment.NewLine;
+            }
+
+            stringArray = textBox4.Lines;
+        }
+
+        #region inpute output select
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            //OpenFileDialog op = new OpenFileDialog();
+            //if (op.ShowDialog() == DialogResult.OK) {
+            //    textsource.Text = op.FileName;
+            //}
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                textsource.Text = folderDialog.SelectedPath;
+            }
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fdb = new FolderBrowserDialog();
+            if (fdb.ShowDialog() == DialogResult.OK)
+            {
+                textdestination.Text= Path.Combine(fdb.SelectedPath,"");
+            }
+        }
+        #endregion
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox4.Text = "";
+            stringArray = null;
+            foreach (object item in checkedListBox1.CheckedItems)
+            {
+                string checkedItemValue = item.ToString();
+                textBox4.Text += checkedItemValue + Environment.NewLine;
+            }
+            stringArray = textBox4.Lines;
+        }
+
+
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+
+            textBox4.Text = "";
+            checkBox1.Checked = false;
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+            progressBar1.Value = 0;
+            label3.Text = "";
+            TimeLabel.Text = "";
+
+        }
+
+        private void contextMenuStrip2_Opening(object sender, CancelEventArgs e)
+        {
+        }
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//private void textBox1_TextChanged_1(object sender, EventArgs e)
+//{
+
+//}
+
+//private void button1_Click_1(object sender, EventArgs e)
+//{
+//    if (string.IsNullOrEmpty(textsource.Text))
+//    {
+//        MessageBox.Show("Select source path");
+//        return;
+//    }
+//    // Get all files and directories in the source folder
+//    string[] filesAndDirectories = Directory.GetFileSystemEntries(textsource.Text);
+
+//    // Concatenate the names into a single string
+//    string result = string.Join(Environment.NewLine, filesAndDirectories);
+
+//    // Set the result to the TextBox
+//    textBox3.Text = result;
+//}
+
+
+
+
+//// Add Button method
+//private void button1_Click_1(object sender, EventArgs e)
+//{
+//    if (string.IsNullOrEmpty(textsource.Text))
+//    {
+//        MessageBox.Show("Select source path");
+//        return;
+//    }
+
+//    string selectedPath = textsource.Text;
+
+//    // Check if the selected path is a folder
+//    if (Directory.Exists(selectedPath))
+//    {
+//        string[] filesAndFolders = Directory.GetFileSystemEntries(selectedPath);
+
+//        checkedListBox1.Items.Clear();
+//        foreach (string path in filesAndFolders)
+//        {
+//            checkedListBox1.Items.Add(path);
+//        }
+//    }
+//    else
+//    {
+//        MessageBox.Show("Please select a valid folder path.");
+//    }
+//}
+
+
+
+
+////convert textbox4 and create string array for file transfer (transfer button)
+//private void checkeditem_Click(object sender, EventArgs e)
+//{
+//    textBox4.Text = "";
+
+//    foreach (object item in checkedListBox1.CheckedItems)
+//    {
+//        string checkedItemValue = item.ToString();
+//        textBox4.Text += checkedItemValue + Environment.NewLine;
+//    }
+//    stringArray = textBox4.Lines;
+//}
+
+//simple method to copy file and folder
